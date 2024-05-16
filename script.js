e12ad0f7252f3ba96f6b861f8333f13379942737
Samuel Umoh <umohsamuel337@gmail.com>
2024-05-16 01:21:42 -0700

    new commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let upBtn = document.getElementById("up-btn2");
+
+// When the user scrolls down 3000px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 3000 || document.documentElement.scrollTop > 2000) {
+    upBtn.style.display = "grid";
+  } else {
+    upBtn.style.display = "none";
+  }
+}
+
+
+
+
